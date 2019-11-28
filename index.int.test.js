7e5d93d74d396016cac416e73c71806f7ed3a793
seongwon <abc7988se@gmail.com>
2019-11-28 16:48:02 +0900

    write inttest

---
+++ index.int.test.js
@@ -0,0 +1,56 @@
+var Application = require("spectron").Application;
+
+const binPath = "testBinary-win32-x64\\testBinary.exe";
+
+
+
+jest.setTimeout(30000);
+
+async function createApp(app){
+    app = new Application({
+        path: binPath
+    });
+
+    await app.start();
+    return app;
+}
+
+
+describe("index.html", ()=> {
+    var app;
+
+    it("starts", async (done)=> {
+        app = await createApp();
+        console.log(app.running);
+        expect(app.running).toBe(true);
+        
+        done();
+    });
+
+    it("is visible", async (done) => {
+        expect(app.running).toBe(true);
+        expect(app.browserWindow).not.toBeUndefined();
+        
+        var isVisible = await app.browserWindow.isVisible();
+        expect(isVisible).toBe(true);
+        done();
+    });
+
+    
+    it("get title", async (done) => {
+        expect(app.running).toBe(true);
+        expect(app.client).not.toBeUndefined();
+
+        var title = await app.client.getTitle();
+        expect(typeof title).toStrictEqual("string");
+        done();
+    });
+    
+    it("stops", async (done)=> {
+        console.log(app.running);
+        expect(app.running).toBe(true);
+        
+        await app.stop();
+        done();
+    });
+});
